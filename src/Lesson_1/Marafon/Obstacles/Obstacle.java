10d0460c7eccfb3037c66e19052ff097c0cf1a7c
LeonidLisin <leo_lis@mail.ru>
2019-06-12 13:13:44 +0300

    Урок №3

--- src/Lesson_1/Marafon/Obstacles/Obstacle.java
+++
@@ -1,8 +0,0 @@
-package Lesson_1.Marafon.Obstacles;
-
-import Lesson_1.Marafon.Competitors.Competitor;
-
-public abstract class Obstacle {
-    public abstract void doIt(Competitor competitor);
-}
-
